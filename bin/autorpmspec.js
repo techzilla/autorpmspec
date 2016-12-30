56fe8e77cc478484f0939ef40b56721bd869dce8
J.M. Becker <jbecker@amtrustgroup.com>
2016-12-30 17:30:08 -0500

    Archive and Spec

--- bin/autorpmspec.js
+++ bin/autorpmspec.js
@@ -4,11 +4,14 @@ var path = require('path')
 var program = require('commander')
 
 var commandPkg = require('../package')
+var prepare = require('../lib/prepare')
+var archive = require('../lib/archive')
+var batch = require('../lib/batch')
 
-var cwd = process.cwd()
+var workingDir = process.cwd()
 
 try {
-  var projectPkg = require(path.resolve(cwd, './package.json'))
+  var projectPkg = require(path.resolve(workingDir, './package.json'))
 } catch (err) {
   console.error('Run within a NodeJS project directory')
   process.exit(1)
@@ -16,11 +19,19 @@ try {
 
 program
   .version(commandPkg.version)
-  .option('-r --release <release>', 'Set release number')
-  .option('-o --output <build>', 'Set output directory')
-  .option('-n --name <name>', 'Set package name')
+  .option('-o, --output <value>', 'Set output directory', 'build')
+  .option('-r --release <value>', 'Set release number', /^(\d+)$/, '1')
+//  .option('-n --name <name>', 'Set package name')
   .parse(process.argv)
 
 // var name = program.name instanceof Function ? undefined : program.name
+var buildDir = path.resolve(workingDir, program.output)
 
-console.log(JSON.stringify(projectPkg, null, 2))
+prepare(buildDir)
+
+archive(workingDir, buildDir, projectPkg)
+
+batch(buildDir, projectPkg)
+
+// console.log(JSON.stringify(projectPkg, null, 2))
+
